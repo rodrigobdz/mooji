629ffc53e38aa64e11dc5e841d8650ef3bb3a20e
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2019-04-16 18:30:56 +0800

    Initial commit
    Add screenshot with example usage, minimal readme and fish plugin.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mooji
+
+> Minimal, beautiful and fast fish prompt
+
+<img src="screenshot.png" width="864">
+
+## Installation
+
+   ```sh
+   fisher add rodrigobdz/mooji
+   ```
+
+## Related Projects
+
+- [brandonweiss/pure.fish](https://github.com/brandonweiss/pure.fish) -
+A [Pure](https://github.com/sindresorhus/pure)-inspired prompt for fish shell
+- [sindresorhus/pure](https://github.com/sindresorhus/pure) - Pretty, minimal and fast ZSH prompt
+
+## Credits
+
+- This readme is based on [rodrigobdz/minimal-readme](https://github.com/rodrigobdz/minimal-readme).
+- Screenshot made with [sindresorhus/iterm2-snazzy](https://github.com/sindresorhus/iterm2-snazzy), [Hyper](https://hyper.is) and [mooji](https://github.com/rodrigobdz/mooji) of course.
+
+## License
+
+[MIT](LICENSE) © [rodrigobdz](https://rodrigobdz.github.io/).
---
+++ fish_prompt.fish
@@ -0,0 +1,54 @@
+set fish_prompt_pwd_dir_length 1
+
+# Fish command and parameter colors
+set fish_color_command green
+set fish_color_param $fish_color_normal
+
+# Git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+
+set __fish_git_prompt_color_branch brown
+set __fish_git_prompt_color_dirtystate FCBC47
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_upstream cyan
+set __fish_git_prompt_color_cleanstate green
+set __fish_git_prompt_color_invalidstate red
+
+# Git Characters
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_cleanstate '✓'
+set __fish_git_prompt_char_stagedstate '⇢'
+set __fish_git_prompt_char_conflictedstate "✕"
+set __fish_git_prompt_char_upstream_prefix ''
+set __fish_git_prompt_char_upstream_equal ''
+set __fish_git_prompt_char_upstream_ahead '⇡'
+set __fish_git_prompt_char_upstream_behind '⇣'
+set __fish_git_prompt_char_upstream_diverged '⇡⇣'
+
+function _print_in_color
+  set -l string $argv[1]
+  set -l color  $argv[2]
+
+  set_color $color
+  printf $string
+  set_color normal
+end
+
+function _prompt_color_for_status
+  if test $argv[1] -eq 0
+    echo magenta
+  else
+    echo red
+  end
+end
+
+function fish_prompt
+  set -l last_status $status
+
+  _print_in_color ""(prompt_pwd) blue
+
+  __fish_git_prompt " (%s)"
+
+  _print_in_color " ❯ " (_prompt_color_for_status $last_status)
+end
